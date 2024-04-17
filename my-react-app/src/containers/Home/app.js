26344b6f7fde6acccaf11cebcaeb184fd5cf7d2a
gust2024 <lombardo.gustavo.2024@gmail.com>
2024-04-17 11:05:56 -0300

    Adicionado Menu - Gustavo

--- my-react-app/src/containers/Home/app.js
+++ my-react-app/src/containers/Home/app.js
@@ -2,3 +2,18 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
