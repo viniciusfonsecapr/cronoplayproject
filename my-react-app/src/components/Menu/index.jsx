0c9a6281ba4ccb6266dbd4f94e660d042b94bb1d
Vinicius Fonseca <vinicius.fonseca20093004@gmail.com>
2024-04-19 02:23:32 -0300

    Ajustes do Menu Tema Dark permanente + Cores DevClub Iniciais

--- my-react-app/src/components/Menu/index.jsx
+++ my-react-app/src/components/Menu/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Logo from "../../assets/logoDevPlay.png";
 import { logout, STORAGE_KEY } from "../../utils/auth";
-
+import { ColorBackground } from "./styles";
 function onSubmit() {
   logout();
   window.location.href = "/login";
@@ -12,34 +12,35 @@ const user = JSON.parse(localStorage.getItem(STORAGE_KEY));
 export function Menu() {
   return (
     <>
-      <nav class="navbar navbar-dark bg-dark fixed-left">
-        <div class="container-fluid">
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
+      <nav class="navbar navbar-dark  border-3 ms-4 mt-4 fixed-top">
+        <div class="container-fluid ">
+          <ColorBackground>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasNavbar"
+              aria-controls="offcanvasNavbar"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </ColorBackground>
           <div
-            class="offcanvas offcanvas-start"
-            tabindex="-1"
+            class="offcanvas offcanvas-start bg-dark"
             id="offcanvasNavbar"
             aria-labelledby="offcanvasNavbarLabel"
           >
-            <div class="offcanvas-header mx-auto ">
-              <nav class=" bg-body-tertiary  ">
-                <div class="">
+            <div class="offcanvas-header mx-auto">
+              <nav class=" bg-body-tertiary">
+                <div class="bg-dark">
                   <a class="" href="#">
                     <img
                       src={Logo}
                       alt="Logo"
-                      width="80"
-                      height="80"
-                      class=""
+                      width="130"
+                      height="130"
+                      class="me-5"
                     />
                   </a>
                 </div>
@@ -47,17 +48,18 @@ export function Menu() {
 
               <button
                 type="button"
-                class="btn-close ps-5"
+                class="btn-close ps-5 ms-5"
                 data-bs-dismiss="offcanvas"
                 aria-label="Close"
               ></button>
             </div>
             <ul class="navbar-nav  w-50 mx-auto mb-5 ">
-              <li class="nav-item mt-5 p-2 mb-3 bg-primary rounded-pill">
+              <li class="nav-item mt-2 p-2 mb-3 bg-primary rounded-pill">
                 <a class="nav-link active ps-3" aria-current="page" href="/">
                   Trilha
                 </a>
               </li>
+
               <li class="nav-item mb-3 p-2 bg-primary rounded-pill">
                 <a class="nav-link  ps-3" href="#">
                   Desafios
@@ -68,18 +70,18 @@ export function Menu() {
                   Conquistas
                 </a>
               </li>
-              <li class="nav-item mb-5 p-2 bg-primary rounded-pill">
+              <li class="nav-item mb-3 p-2 bg-primary rounded-pill">
                 <a class="nav-link  ps-3" href="#">
                   Mais
                 </a>
               </li>
             </ul>
 
-            <div class=" d-flex flex-row bg-body-tertiary mt-5 mx-auto">
+            <div class=" d-flex flex-row bg-dark text-light mt-1 mx-auto">
               <img src={Logo} alt="Logo" width="100" height="100" class="" />
-              <div class="d-flex flex-column  ms-5">
-                <p class="m-auto">{user.name}</p>
-                <p class="m-auto">Nivel 5</p>
+              <div class="d-flex flex-column  ms-5 text-uppercase fw-bold">
+                <p class="m-auto ">{user.name}</p>
+                <p class="m-auto ">Nivel 5</p>
               </div>
             </div>
 
