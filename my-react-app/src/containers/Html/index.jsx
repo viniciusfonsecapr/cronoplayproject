1e6f954b3386b6dce9860b843f39d3cc5479ebf3
Vinicius Fonseca <vinicius.fonseca20093004@gmail.com>
2024-04-29 01:02:07 -0300

    Tela 1 Html - Ajustes Iniciais

--- my-react-app/src/containers/Html/index.jsx
+++ my-react-app/src/containers/Html/index.jsx
@@ -1,12 +1,35 @@
 import React from "react";
-import { Container } from "./styles.js";
+import { Container, ContainerDiv, AlignLi } from "./styles.js";
 import { Menu } from "../../components/Menu/index.jsx";
+import { FasesHtml } from "./FasesHtml/index.jsx";
 
 export function Html() {
   return (
     <>
       <Container>
         <Menu></Menu>
+        <ContainerDiv>
+          <h1>Missão 1</h1>
+          <ul>
+            <li>
+              {" "}
+              <input type="checkbox" name="" id="" />
+            </li>
+            <li>
+              {" "}
+              <input type="checkbox" name="" id="" />
+            </li>
+            <li>
+              {" "}
+              <input type="checkbox" name="" id="" />
+            </li>
+            <li>
+              {" "}
+              <input type="checkbox" name="" id="" />
+            </li>
+          </ul>
+        </ContainerDiv>
+        <FasesHtml />
       </Container>
     </>
   );
--- my-react-app/src/containers/Html/styles.js
+++ my-react-app/src/containers/Html/styles.js
@@ -1,7 +1,50 @@
 import styled from "styled-components";
+import Background from '../../assets/stepOne/background.png'
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  background: url('${Background}');
+  background-repeat: no-repeat;
+  background-size: cover;
+  
 `;
 
+
+export const ContainerDiv = styled.div`
+position: absolute;
+margin-top: 12%;
+margin-left: 30%;
+width: 700px;
+height: 500px;
+background: #9b4f06;
+border-radius: 50px;
+  
+
+h1 {
+  text-align: center;
+  padding-top: 20px;
+  color: orange;
+  font-weight: 900;
+}
+ul {
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  padding-top: 50px;
+}
+li {
+  color: orange;
+  text-decoration: none;
+  list-style-type: none;
+}
+
+input {
+   width: 40px;
+   height: 40px;
+}
+
+`
+
+export const AlignLi = styled.div`
+`
